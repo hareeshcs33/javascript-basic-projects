580f199b301636202967375e1217701495d193bc
hareeshcs33 <hareeshcs33@gmail.com>
2020-11-24 13:41:11 +0530

    tabs project added

---
+++ 11-tabs/setup/app.js
@@ -0,0 +1,21 @@
+const btns = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener('click', function (e) {
+    console.log(e.target);
+    const id = e.target.dataset.id;
+    if (id) {
+        btns.forEach(function (btn) {
+            //remove active from other buttons
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+        // hide other articles
+        articles.forEach(function (article) {
+            article.classList.remove("active");
+        });
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    }
+})
